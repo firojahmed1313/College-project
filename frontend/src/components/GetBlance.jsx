c4c1c257f5f4d148b12d4f8bbac5d932c8b10982
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-03-03 15:39:29 +0000

    convert inr

--- frontend/src/components/GetBlance.jsx
+++ frontend/src/components/GetBlance.jsx
@@ -1,17 +1,28 @@
 import React, { useContext, useState } from 'react'
 import context from '../context/Context'
 import { ethers } from 'ethers';
+import axios from 'axios';
 const GetBlance = () => {
     const [blance, setBlance] = useState();
+    const [inrblance, setInrBlance] = useState();
+    const [inrConvert, setInrConvert] = useState();
     const [button, setButton] = useState(true);
     const auth = useContext(context);
     const { provider} = auth.state;
     console.log(auth.account);
     const getBlance =async () => {
-
+        try {
+            const data= await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=ethereum");
+            console.log(data.data[0].current_price);
+            setInrConvert(data.data[0].current_price)
+        } catch (error) {
+            console.warn(error);
+        }
         setButton(false);
         const balanceBigInt = await provider.getBalance(auth.account);
+
         setBlance(ethers.formatEther(balanceBigInt));
+        setInrBlance((ethers.formatEther(balanceBigInt))*inrConvert);
 
 
     }
@@ -23,7 +34,7 @@ const GetBlance = () => {
         <>
             <div className='getBlanceContainer'>
                 <h3>Your Blance Is :</h3>
-                {(button)?<button className='BlanceButton' onClick={getBlance}> Chack Blance</button>:<h2>{blance} ETH</h2>}
+                {(button)?<button className='BlanceButton' onClick={getBlance}> Chack Blance</button>:<h2>{Number(blance).toFixed(2)} Eth  ,  {Number(inrblance).toFixed(2)} Inr</h2>}
             </div >
         </>
     )
